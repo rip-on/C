653d75ebe0d80c0b112f8decf92a7311b17ddab1
rip-on <shipon.ripon123@gmail.com>
2025-03-19 11:06:25 +0600

    Swapping character in C

---
+++ swap_char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    char a = 'A';
+    char b = 'B';
+    char temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("%c\n",a);
+    printf("%c\n",b);
+    return 0;
+}
