3b7f4eb0180115756a8d3062fe6477be1644ba27
rip-on <shipon.ripon123@gmail.com>
2025-03-19 11:13:02 +0600

    Intro to Myself

---
+++ myself.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int age = 22;
+    char name[] = "Ripon";
+    float gpa = 2.08;
+    char grade = 'C';
+
+    printf("My name is %s\n",name);
+    printf("I am %d years old\n",age);
+    printf("My average gpa is %f\n",gpa);
+    printf("My grade is %c",grade);
+    return 0;
+}
