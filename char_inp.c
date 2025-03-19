a828e491bc39a1ff246a06a433704953d93c0957
rip-on <shipon.ripon123@gmail.com>
2025-03-19 10:21:06 +0600

    Name input in C (%s)

---
+++ char_inp.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    // char name[25];
+    // int age;
+
+    // printf("What's is your name?: ");
+    // printf("How old are you?:\n");
+    // scanf("%s ", &name);
+    // scanf("%d",&age);
+    // printf("\nHello %s ! ",name);
+    // printf("\nYou are %d years old.",age);
+
+    // return 0;
+    
+
+    char name[25];
+    int age;
+
+    printf("\nWhat's your name?:");
+    scanf("%s",&name);
+
+    printf("How old are you?:");
+    scanf("%d",&age);
+
+    printf("Hi %s! How are You?\n",name);
+    printf("You are %d years old\n",age);
+
+    return 0;
+}
