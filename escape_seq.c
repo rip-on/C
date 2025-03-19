a99c385f5ddcb6c94d0ab2cc83eb44704d82d123
rip-on <shipon.ripon123@gmail.com>
2025-03-19 11:02:23 +0600

    Escape Sequence

---
+++ escape_seq.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("I love Pizza!\n");
+    printf("\"I love Pizza\"\n");
+    printf("\'My name is Harry\'\n");
+    printf("Hi\a\n");
+    printf("Hi Jennifer\b\n");
+    printf("\?HUh\?\n");
+    printf("1\t2\t3\t\n4\t5\t6\t\n7\t8\t9\n");
+    char name[]="HEY Jahid";
+    printf("%s\n",name);
+    return 0;
+}
